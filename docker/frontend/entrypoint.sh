517acd0c0abf5778a77549dd570b66ae34e2955d
MrEndor <mrendorer@gmail.com>
2025-01-18 00:53:13 +0300

    build(docker): create dockerfile for frontend

---
+++ docker/frontend/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly cmd="$*"
+
+backend () {
+  dockerize -wait 'tcp://backend:8000' -timeout 5s
+}
+
+until backend; do
+  >&2 echo 'backend service is unavailable - sleeping'
+done
+
+>&2 echo 'backend service is up - continuing...'
+
+exec $cmd
